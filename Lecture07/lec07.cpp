f8bf496683c9791aed274412ff11fc884d9a10be
James Tuck <jtuck@ncsu.edu>
2018-09-19 20:39:12 -0400

    Lecture 7 code.

---
+++ Lecture07/lec07.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+class Rectangle {
+private:
+    int width;
+    int length;
+public:
+    Rectangle(int w, int l):width(w),length(l) {}
+    int area() { return width * length; }
+    virtual void print() { printf("Rectangle"); }
+};
+
+class Square : public Rectangle {
+public:
+    Square(int s):Rectangle(s,s){}
+    void print() { printf("Square"); }
+
+};
+
+/*Rectangle::Rectangle(int w, int l)
+{
+  width = w;
+  length = l;
+}*/
+/*
+Square::Square(int side)
+:Rectangle(side,side)
+{
+}*/
+
+int main()
+{
+  Square s(5);
+  Rectangle r(4,6);
+
+  Rectangle &ref = s;
+
+  printf("area of s = %d\n", ref.area());
+  printf("area of r = %d\n", r.area());
+  printf("\n");
+  ref.print();printf("\n");
+  r.print();printf("\n");
+  return 0;
+}
