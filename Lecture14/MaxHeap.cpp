b3fe89c0aa90c2fbbc6fb6b4cbd58309e97f91e9
James Tuck <jtuck@ncsu.edu>
2018-10-17 19:01:27 -0400

    Lecture 14 code for Max-Heap

---
+++ Lecture14/MaxHeap.cpp
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+class MaxHeap {
+private:
+  int *heapArray;    // this is the maxheap
+  int heapArraySize; // size of the array
+  int nextIndex;     // location of next free array entry
+
+protected:
+  void percolate_up(int);
+  void percolate_down(int, int);
+
+public:
+  MaxHeap(int size = 100) : heapArraySize(size) {
+    heapArray = new int[size];
+    for (int i = 0; i < size; i++)
+      heapArray[i] = 0;
+    nextIndex = 0;
+  }
+
+  void print() {
+    for (int i = 0; i < nextIndex; i++) {
+      printf("%d ", heapArray[i]);
+    }
+    printf("\n");
+  }
+
+  bool insert(int);
+  bool remove(int &); // remove the max value
+};
+
+void MaxHeap::percolate_up(int index) {
+  while (index > 0) {
+    int parent_index = (index - 1) / 2;
+    if (heapArray[index] > heapArray[parent_index]) {
+      int tmp = heapArray[index];
+      heapArray[index] = heapArray[parent_index];
+      heapArray[parent_index] = tmp;
+      index = parent_index;
+    } else
+      return;
+  }
+}
+
+void MaxHeap::percolate_down(int index, int size) {
+  int child_index = index * 2 + 1;
+  int val = heapArray[index];
+  while (child_index < size) {
+    int max_val = val;
+    int max_index = -1;
+    for (int i = child_index; i < child_index + 2; i++) {
+      if (heapArray[i] > max_val) {
+        max_val = heapArray[i];
+        max_index = i;
+      }
+    }
+    if (max_val == val)
+      return;
+    heapArray[index] = max_val;
+    heapArray[max_index] = val;
+    index = max_index;
+    child_index = index * 2 + 1;
+  }
+}
+
+bool MaxHeap::insert(int data) {
+  if (nextIndex < heapArraySize) {
+    heapArray[nextIndex] = data;
+    percolate_up(nextIndex);
+    nextIndex++;
+    return true;
+  } else {
+    return false;
+  }
+}
+
+bool MaxHeap::remove(int &val) {
+  if (nextIndex > 0) {
+    val = heapArray[0];
+    heapArray[0] = heapArray[nextIndex - 1];
+    nextIndex -= 1;
+    percolate_down(0, nextIndex);
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  MaxHeap heap;
+
+  heap.insert(45);
+  heap.print();
+
+  heap.insert(100);
+  heap.print();
+
+  heap.insert(5);
+  heap.insert(50);
+  heap.print();
+
+  int ref;
+  heap.remove(ref);
+  heap.print();
+
+  return 0;
+}
